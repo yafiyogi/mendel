eaa21b93a37d3cee96c7ea0fc058a053681a15e1
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-03-09 18:10:35 +0000

    Add metric property location label_handler.

--- configure_values.cpp
+++ configure_values.cpp
@@ -47,6 +47,7 @@
 #include "mqtt_handler.h"
 #include "values_config.h"
 #include "values_metric.h"
+#include "values_metric_labels.hpp"
 
 #include "label_action_copy.h"
 #include "label_action_drop.h"
@@ -80,75 +81,79 @@ constexpr const auto g_value_action_types =
 LabelActions configure_label_actions(const YAML::Node & yaml_label_actions)
 {
   LabelActions label_actions;
-  label_actions.reserve(yaml_label_actions.size());
 
-  for(const auto & yaml_label_action : yaml_label_actions)
+  if(yaml_label_actions)
   {
-    auto action_name{yy_util::to_lower(yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["action"sv])))};
-    LabelActionPtr action;
+    label_actions.reserve(yaml_label_actions.size());
 
-    spdlog::info("       - action [{}]."sv, action_name);
-    spdlog::trace("          [line {}]."sv, yaml_label_action.Mark().line + 1);
-    switch(g_label_action_types.lookup(action_name))
+    for(const auto & yaml_label_action : yaml_label_actions)
     {
-      case LabelActionType::Copy:
+      auto action_name{yy_util::to_lower(yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["action"sv])))};
+      LabelActionPtr action;
+
+      spdlog::info("       - action [{}]."sv, action_name);
+      spdlog::trace("          [line {}]."sv, yaml_label_action.Mark().line + 1);
+      switch(g_label_action_types.lookup(action_name))
       {
-        std::string_view source{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["source"sv]))};
-        std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
-        if(!source.empty()
-           || !target.empty())
+        case LabelActionType::Copy:
         {
-          action = yy_util::static_unique_cast<LabelAction>(std::make_unique<CopyLabelAction>(std::string{source},
-                                                                                              std::string{target}));
+          std::string_view source{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["source"sv]))};
+          std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
+          if(!source.empty()
+             || !target.empty())
+          {
+            action = yy_util::static_unique_cast<LabelAction>(std::make_unique<CopyLabelAction>(std::string{source},
+                                                                                                std::string{target}));
+          }
         }
-      }
-      break;
-
-      case LabelActionType::Drop:
-      {
-        std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
-        if(!target.empty())
-        {
-          action = yy_util::static_unique_cast<LabelAction>(std::make_unique<DropLabelAction>(std::string{target}));
-        }
-      }
-      break;
-
-      case LabelActionType::Keep:
-      {
-        std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
-        if(!target.empty())
-        {
-          action = yy_util::static_unique_cast<LabelAction>(std::make_unique<KeepLabelAction>(std::string{target}));
-        }
-      }
-      break;
-
-      case LabelActionType::ReplacePath:
-      {
-        std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
-
-        if(!target.empty())
-        {
-          auto create_topics = [&yaml_label_action]() {
-            return configure_label_action_replace_path(yaml_label_action["replace"sv]);
-          };
-
-          action = yy_util::static_unique_cast<LabelAction>(std::make_unique<ReplacePathLabelAction>(std::string{target},
-                                                                                                     create_topics()));
-        }
-      }
-      break;
-
-      default:
-        spdlog::warn("Unrecognized action [{}]"sv, action_name);
-        spdlog::trace("  [line {}]."sv, yaml_label_action.Mark().line + 1);
         break;
-    }
 
-    if(action)
-    {
-      label_actions.emplace_back(std::move(action));
+        case LabelActionType::Drop:
+        {
+          std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
+          if(!target.empty())
+          {
+            action = yy_util::static_unique_cast<LabelAction>(std::make_unique<DropLabelAction>(std::string{target}));
+          }
+        }
+        break;
+
+        case LabelActionType::Keep:
+        {
+          std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
+          if(!target.empty())
+          {
+            action = yy_util::static_unique_cast<LabelAction>(std::make_unique<KeepLabelAction>(std::string{target}));
+          }
+        }
+        break;
+
+        case LabelActionType::ReplacePath:
+        {
+          std::string_view target{yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_label_action["target"sv]))};
+
+          if(!target.empty())
+          {
+            auto create_topics = [&yaml_label_action]() {
+              return configure_label_action_replace_path(yaml_label_action["replace"sv]);
+            };
+
+            action = yy_util::static_unique_cast<LabelAction>(std::make_unique<ReplacePathLabelAction>(std::string{target},
+                                                                                                       create_topics()));
+          }
+        }
+        break;
+
+        default:
+          spdlog::warn("Unrecognized action [{}]"sv, action_name);
+          spdlog::trace("  [line {}]."sv, yaml_label_action.Mark().line + 1);
+          break;
+      }
+
+      if(action)
+      {
+        label_actions.emplace_back(std::move(action));
+      }
     }
   }
 
@@ -158,102 +163,120 @@ LabelActions configure_label_actions(const YAML::Node & yaml_label_actions)
 ValueActions configure_value_actions(const YAML::Node & yaml_value_actions)
 {
   ValueActions value_actions;
-  if(!yaml_value_actions)
+  if(yaml_value_actions)
   {
-    return value_actions;
-  }
+    value_actions.reserve(yaml_value_actions.size());
 
-  value_actions.reserve(yaml_value_actions.size());
-
-  for(const auto & yaml_value_action : yaml_value_actions)
-  {
-    auto action_name{yy_util::to_lower(yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_value_action["action"sv])))};
-    ValueActionPtr action;
-
-    spdlog::info("       - action [{}]."sv, action_name);
-    spdlog::trace("          [line {}]."sv, yaml_value_action.Mark().line + 1);
-    switch(g_value_action_types.lookup(action_name, ValueActionType::Keep))
+    for(const auto & yaml_value_action : yaml_value_actions)
     {
-      case ValueActionType::Keep:
-        // Don't add as it does nothing.
-        // action = yy_util::static_unique_cast<ValueAction>(std::make_unique<KeepValueAction>());
-        break;
+      auto action_name{yy_util::to_lower(yy_util::trim(yy_util::yaml_get_value<std::string_view>(yaml_value_action["action"sv])))};
+      ValueActionPtr action;
 
-      case ValueActionType::Switch:
+      spdlog::info("       - action [{}]."sv, action_name);
+      spdlog::trace("          [line {}]."sv, yaml_value_action.Mark().line + 1);
+      switch(g_value_action_types.lookup(action_name, ValueActionType::Keep))
       {
-        auto & yaml_values = yaml_value_action["values"sv];
+        case ValueActionType::Keep:
+          // Don't add as it does nothing.
+          // action = yy_util::static_unique_cast<ValueAction>(std::make_unique<KeepValueAction>());
+          break;
 
-        if(yaml_values)
+        case ValueActionType::Switch:
         {
-          std::optional<std::string> default_value{std::nullopt};
-          SwitchValueAction::Switch switch_values;
+          auto & yaml_values = yaml_value_action["values"sv];
 
-          if(auto num_cases = yaml_values.size();
-             num_cases > 1)
+          if(yaml_values)
           {
-            switch_values.reserve(num_cases - 1);
-          }
+            std::optional<std::string> default_value{std::nullopt};
+            SwitchValueAction::Switch switch_values;
 
-          for(auto & yaml_case : yaml_values)
-          {
-            if(!yy_util::yaml_is_scalar(yaml_case))
+            if(auto num_cases = yaml_values.size();
+               num_cases > 1)
             {
-              if(!default_value.has_value())
+              switch_values.reserve(num_cases - 1);
+            }
+
+            for(auto & yaml_case : yaml_values)
+            {
+              if(!yy_util::yaml_is_scalar(yaml_case))
               {
-                if(auto & yaml_default = yaml_case["default"sv];
-                   yaml_default)
+                if(!default_value.has_value())
                 {
-                  default_value = yy_util::yaml_get_optional_value<std::string>(yaml_default);
-                  if(default_value.has_value())
+                  if(auto & yaml_default = yaml_case["default"sv];
+                     yaml_default)
                   {
-                    spdlog::info("         - default: [{}]", default_value.value());
+                    default_value = yy_util::yaml_get_optional_value<std::string>(yaml_default);
+                    if(default_value.has_value())
+                    {
+                      spdlog::info("         - default: [{}]", default_value.value());
+                    }
+                  }
+                }
+
+                if(auto & yaml_input = yaml_case["input"sv];
+                   yaml_input)
+                {
+                  if(auto & yaml_output = yaml_case["output"sv];
+                     yaml_output)
+                  {
+                    auto input{yy_util::yaml_get_value<std::string_view>(yaml_input)};
+                    auto output{yy_util::yaml_get_value<std::string_view>(yaml_output)};
+
+                    spdlog::info("         - input: [{}] output: [{}]", input, output);
+                    switch_values.emplace(std::string{input},
+                                          std::string{output});
                   }
                 }
               }
+            }
 
-              if(auto & yaml_input = yaml_case["input"sv];
-                 yaml_input)
-              {
-                if(auto & yaml_output = yaml_case["output"sv];
-                   yaml_output)
-                {
-                  auto input{yy_util::yaml_get_value<std::string_view>(yaml_input)};
-                  auto output{yy_util::yaml_get_value<std::string_view>(yaml_output)};
-
-                  spdlog::info("         - input: [{}] output: [{}]", input, output);
-                  switch_values.emplace(std::string{input},
-                                        std::string{output});
-                }
-              }
+            if(default_value.has_value()
+               && !switch_values.empty())
+            {
+              action = yy_util::static_unique_cast<ValueAction>(std::make_unique<SwitchValueAction>(std::move(default_value.value()),
+                                                                                                    std::move(switch_values)));
             }
           }
-
-          if(default_value.has_value()
-             && !switch_values.empty())
-          {
-            action = yy_util::static_unique_cast<ValueAction>(std::make_unique<SwitchValueAction>(std::move(default_value.value()),
-                                                                                                  std::move(switch_values)));
-          }
         }
-      }
-      break;
-
-      default:
-        spdlog::debug("configure_value_actions(): 4c");
-        spdlog::warn("Unrecognized action [{}]"sv, action_name);
-        spdlog::trace("  [line {}]."sv, yaml_value_action.Mark().line + 1);
         break;
-    }
 
-    if(action)
-    {
-      value_actions.emplace_back(std::move(action));
+        default:
+          spdlog::debug("configure_value_actions(): 4c");
+          spdlog::warn("Unrecognized action [{}]"sv, action_name);
+          spdlog::trace("  [line {}]."sv, yaml_value_action.Mark().line + 1);
+          break;
+      }
+
+      if(action)
+      {
+        value_actions.emplace_back(std::move(action));
+      }
     }
   }
 
   return value_actions;
 }
 
+LabelActions configure_value_properties(const YAML::Node & yaml_value)
+{
+  LabelActions l_property_actions{};
+
+  // Configure 'location' property.
+  {
+    auto create_location = [&yaml_value]() {
+      ReplacementTopicsConfig topics_config{};
+
+      configure_label_action_replace_path_format(yaml_value[values::g_label_location], topics_config);
+
+      return topics_config.create_automaton();
+    };
+
+    l_property_actions.emplace_back(yy_util::static_unique_cast<LabelAction>(std::make_unique<ReplacePathLabelAction>(std::string{values::g_label_location},
+                                                                                                                      create_location())));
+  }
+
+  return l_property_actions;
+}
 
 } // anonymous namespace
 
@@ -302,10 +325,15 @@ MetricsMap configure_values(const YAML::Node & yaml_values)
               return configure_value_actions(yaml_handler["value_actions"sv]);
             };
 
-            auto metric{std::make_shared<Metric>(MetricId{value_id, Labels{}},
+            auto create_property_actions = [&yaml_handler]() {
+              return configure_value_properties(yaml_handler);
+            };
+
+            auto metric{std::make_shared<Metric>(MetricId{value_id, std::string{}},
                                                  std::string{property_name.value()},
                                                  create_label_actions(),
-                                                 create_value_actions())};
+                                                 create_value_actions(),
+                                                 create_property_actions())};
 
             spdlog::info("     - add metric [{}] to handler [{}] property [{}]."sv,
                          metric->Id().Id(),
